e910b90314008318f361de8db2edccd963309463
eee <maxzms@mail.ru>
2018-09-10 02:01:06 +0400

    Draft DFA

---
+++ Automaton.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Finite_Automaton
+{
+    public class Automaton
+    {
+        private String _inputSignals;
+        private Dictionary<String, Dictionary<String, String>> _table;
+        private SortedSet<String> _s;
+        private SortedSet<String> _f;
+
+        public Automaton(String inputSignals, Dictionary<String, Dictionary<String, String>> table,
+            SortedSet<String> s, SortedSet<String> f)
+        {
+            _inputSignals = inputSignals;
+            _table = table;
+            _s = s;
+            _f = f;
+        }
+
+        public Tuple<bool, int> MaxString(String inputWord, int k)
+        {
+            Tuple<bool, int> result = new Tuple<bool, int>(_s.Intersect(_f).Count() == 0 ? false : true, 0);
+
+
+            SortedSet<String> state = new SortedSet<String>(_s);
+
+            // состояние лучше классом или структурой
+
+            for (int i = k; i < inputWord.Length; i++)
+            {
+                SortedSet<String> tmp = new SortedSet<String>();
+                foreach (var cur_state in state)
+                {
+                    tmp.Add(_table[cur_state][new String(inputWord[i], 1)]);
+                }
+
+                result = new Tuple<bool, int>(_s.Intersect(_f).Count() == 0 ? false : true, i - k);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finite_Automaton
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Automaton a = new Automaton();
+
+            //String sigma = "abcdef";
+            //int nstate = 8; // кол-во состояний (т.е. мощность множества Q)
+
+            //Dictionary<string, Dictionary<string, string>> delta; // таблица состояний автомата
+            // первый ключ - состояний
+            // второй ключ - сигнал
+            // значение словаря - новое состояние автомата
+
+            //SortedSet<string> S; // множество начальных состояний автомата
+            //SortedSet<string> F; // множество конечных состоний
+
+            using (StreamReader inp = new StreamReader(@"C:\Users\Morpheus\source\repos\Finite_Automaton\Finite_Automaton\input.txt"))
+            {
+                char[] delim = { ' ' };
+
+                String sigma = inp.ReadLine().Trim();
+
+                int nstate = int.Parse(inp.ReadLine());
+                // skip
+                inp.ReadLine();
+
+                var delta = new Dictionary<string, Dictionary<string, string>>();
+                for (int i = 0; i < nstate; i++)
+                {
+                    String[] line = inp.ReadLine().Split(delim, StringSplitOptions.RemoveEmptyEntries);
+                    delta.Add(line[0], new Dictionary<string, string>());
+
+                    for (int j = 0; j < sigma.Length; j++)
+                    {
+                        delta[line[0]].Add(new String(sigma[j], 1), line[j]);
+                    }
+                }
+
+                inp.ReadLine(); // blank line
+
+                SortedSet<String> S = new SortedSet<string>();
+                foreach (var q in inp.ReadLine().Split(delim, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    S.Add(q);
+                }
+
+
+                SortedSet<String> F = new SortedSet<string>();
+                foreach (var q in inp.ReadLine().Split(delim, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    F.Add(q);
+                }
+
+
+                int k = int.Parse(inp.ReadLine());
+
+
+                String input_word = inp.ReadLine().Trim();
+
+                Automaton a = new Automaton(sigma, delta, S, F);
+
+                Tuple<bool, int> res = a.MaxString(input_word, k);
+
+                if (res.Item1)
+                {
+                    Console.WriteLine($"Answer is: <True, {res.Item2}>");
+                }
+                else
+                {
+                    Console.WriteLine("Answer is: <False, 0>");
+                }
+            }
+        }
+    }
+}
